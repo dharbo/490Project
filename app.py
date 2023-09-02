12bcc1d58a6a88c014cb7fbde1c6d256b9e9c64b
dharbo <harboyandavid@gmail.com>
2023-09-02 14:26:21 -0700

    Account Creation/Login

--- app.py
+++ app.py
@@ -1,22 +1,81 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+
+import sqlite3
+
+# Set up the database
+DATABASE = './database.db'
+
+connection = sqlite3.connect(DATABASE)
+cursor = connection.cursor()
+
+cursor.execute("CREATE TABLE IF NOT EXISTS USER (User_Name TEXT PRIMARY KEY, Password TEXT)")
+
+connection.close()
+
+### Need a way to check if a user is logged in. If they aren't they shouldn't be able to access the home page
+user_logged_in = False
 
 app = Flask('__name__')
 
 @app.route('/')
 def index():
-    return render_template('index.html', title = 'Landing Page')
+    return render_template('index.html', title='Landing Page')
 
-@app.route('/signup')
+@app.route('/signup', methods=["GET", "POST"])
 def signup():
-    return render_template('signup.html', title = 'Signup')
+    if request.method == 'POST':
 
-@app.route('/login')
+        # Get the data from the form
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        connection = sqlite3.connect(DATABASE)
+        cursor = connection.cursor()
+
+        row = cursor.execute("SELECT User_Name FROM USER WHERE User_Name=?", (username,)).fetchone()
+
+        # If row does not exist in the database, insert the data. Otherwise make the user retry.
+        if row is None:
+            cursor.execute("INSERT INTO USER VALUES (?, ?)", (username, password,))
+            connection.commit()
+            connection.close()
+            return redirect('home')
+
+        else:
+            connection.close()
+            return render_template('signup.html', title='Signup', retry=True)
+    
+    return render_template('signup.html', title='Signup')  # This is a get, should there be an if statement?
+
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template('login.html', title = 'Login')
+    if request.method == 'POST':
+
+        # Get the data from the form
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        connection = sqlite3.connect(DATABASE)
+        cursor = connection.cursor()
+
+        row = cursor.execute("SELECT User_Name, Password FROM USER WHERE User_Name=? AND Password=?", (username, password,)).fetchone()
+
+        # If row exists, then user successfully logged in. Otherwise, the inputted username or password was incorrect.
+        if row is not None:
+            user_logged_in = True
+            connection.close()
+
+            return redirect('home')
+
+        else:
+            connection.close()
+            return render_template('login.html', title='Login', retry=True)
+
+    return render_template('login.html', title='Login')  # This is a get, should there be an if statement?
 
 @app.route('/home')
 def home():
-    return render_template('home.html', title = 'Home', homepage="true")
+    return render_template('home.html', title='Home', homepage="true")
 
 if __name__ == '__main__':
     app.run(debug=True)
