412e6d3500fbad64f0facdf107f4e43a9ffb5d4c
dharbo <harboyandavid@gmail.com>
2023-11-18 20:14:24 -0800

    Removed console logs from javascript and fixed formatting for html

--- static/js/categoryModal.js
+++ static/js/categoryModal.js
@@ -1,130 +1,89 @@
-const closeModalBtns = document.querySelectorAll("#close-create-category-modal, #close-update-category-modal, #close-delete-category-modal, #close-add-transaction-modal, #close-update-transaction-modal, #close-delete-transaction-modal")
+const closeModalBtns = document.querySelectorAll("#close-create-category-modal, #close-update-category-modal, \
+                                                  #close-delete-category-modal, #close-add-transaction-modal, \
+                                                  #close-update-transaction-modal, #close-delete-transaction-modal");
 
-console.log(closeModalBtns)
-
+// Add event listeners on each button that closes a modal
 closeModalBtns.forEach(btn => {
   btn.addEventListener("click", () => {
-    console.log(btn.getAttribute("id"))
-
     if (btn === document.getElementById("close-create-category-modal")) {
-      modal = document.getElementById("create-category-modal")
-      modal.classList.remove("visible")
-      modal.classList.add("hidden")
+      modal = document.getElementById("create-category-modal");
+      modal.classList.remove("visible");
+      modal.classList.add("hidden");
     } else if (btn === document.getElementById("close-update-category-modal")) {
-      modal = document.getElementById("update-category-modal")
-      modal.classList.remove("visible")
-      modal.classList.add("hidden")
+      modal = document.getElementById("update-category-modal");
+      modal.classList.remove("visible");
+      modal.classList.add("hidden");
     } else if (btn === document.getElementById("close-delete-category-modal")) {
-      modal = document.getElementById("delete-category-modal")
-      modal.classList.remove("visible")
-      modal.classList.add("hidden")
+      modal = document.getElementById("delete-category-modal");
+      modal.classList.remove("visible");
+      modal.classList.add("hidden");
     } else if (btn === document.getElementById("close-add-transaction-modal")) {
-      modal = document.getElementById("add-transaction-modal")
-      modal.classList.remove("visible")
-      modal.classList.add("hidden")
+      modal = document.getElementById("add-transaction-modal");
+      modal.classList.remove("visible");
+      modal.classList.add("hidden");
     } else if (btn === document.getElementById("close-update-transaction-modal")) {
-      modal = document.getElementById("update-transaction-modal")
-      modal.classList.remove("visible")
-      modal.classList.add("hidden")
+      modal = document.getElementById("update-transaction-modal");
+      modal.classList.remove("visible");
+      modal.classList.add("hidden");
     } else if (btn === document.getElementById("close-delete-transaction-modal")) {
-      modal = document.getElementById("delete-transaction-modal")
-      modal.classList.remove("visible")
-      modal.classList.add("hidden")
+      modal = document.getElementById("delete-transaction-modal");
+      modal.classList.remove("visible");
+      modal.classList.add("hidden");
     }
-
-    return
+    return;
   })
 });
 
 // Open create-category-modal
 document.getElementById("create-category").addEventListener("click", () => {
-  // open modal
-  console.log("Open modal...")
-
-  modal = document.getElementById("create-category-modal")
-  modal.classList.remove("hidden")
-  modal.classList.add("visible")
-
-  console.log("It work!!!")
-
-  return
+  modal = document.getElementById("create-category-modal");
+  modal.classList.remove("hidden");
+  modal.classList.add("visible");
+  return;
 });
 
 // Open update-category-modal
 document.getElementById("update-category").addEventListener("click", () => {
-  // open modal
-  console.log("Open modal...")
-
-  modal = document.getElementById("update-category-modal")
-  modal.classList.remove("hidden")
-  modal.classList.add("visible")
-
-  console.log("It work!!!")
-
-  return
+  modal = document.getElementById("update-category-modal");
+  modal.classList.remove("hidden");
+  modal.classList.add("visible");
+  return;
 });
 
 // Open delete-category-modal
 document.getElementById("delete-category").addEventListener("click", () => {
-  // open modal
-  console.log("Open modal...")
-
-  modal = document.getElementById("delete-category-modal")
-  modal.classList.remove("hidden")
-  modal.classList.add("visible")
-
-  console.log("It work!!!")
-
-  return
+  modal = document.getElementById("delete-category-modal");
+  modal.classList.remove("hidden");
+  modal.classList.add("visible");
+  return;
 });
 
 // Open add-transaction-modal
 document.getElementById("add-transaction").addEventListener("click", () => {
-  // open modal
-  console.log("Open modal...")
-
-  modal = document.getElementById("add-transaction-modal")
-  modal.classList.remove("hidden")
-  modal.classList.add("visible")
-
-  console.log("It work!!!")
-
-  return
+  modal = document.getElementById("add-transaction-modal");
+  modal.classList.remove("hidden");
+  modal.classList.add("visible");
+  return;
 });
 
 // Open update-transaction-modal
 document.getElementById("update-transaction").addEventListener("click", () => {
-  // open modal
-  console.log("Open modal...")
-
-  modal = document.getElementById("update-transaction-modal")
-  modal.classList.remove("hidden")
-  modal.classList.add("visible")
-
-  console.log("It work!!!")
-
-  return
+  modal = document.getElementById("update-transaction-modal");
+  modal.classList.remove("hidden");
+  modal.classList.add("visible");
+  return;
 });
 
 // Open delete-transaction-modal
 document.getElementById("delete-transaction").addEventListener("click", () => {
-  // open modal
-  console.log("Open modal...")
-
-  modal = document.getElementById("delete-transaction-modal")
-  modal.classList.remove("hidden")
-  modal.classList.add("visible")
-
-  console.log("It work!!!")
-
-  return
+  modal = document.getElementById("delete-transaction-modal");
+  modal.classList.remove("hidden");
+  modal.classList.add("visible");
+  return;
 });
 
+// Remove alert message element
 document.getElementById("alert-message-btn").addEventListener("click", () => {
-  console.log('closing alert')
-
-  alertMessage = document.getElementById("alert-message")
-
-  alertMessage.remove()
-
-})
+  alertMessage = document.getElementById("alert-message");
+  alertMessage.remove();
+});
